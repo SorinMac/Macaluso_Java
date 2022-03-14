2cb97d2d7b43e7610b4ed3d40f344def801d254d
Juan Arias <juan.arias@marist.edu>
2022-03-13 21:24:51 -0400

    Feedback

--- prj/1/project1.java
+++ prj/1/project1.java
@@ -2,32 +2,30 @@
 //hw3
 //CMPT 220 
 //Problem4.java
+// JA: What does the program do?
 
 import java.util.Scanner;
 
 public class project1 {
-    public static void main(String[] args){
-        //creates the scanner
+    public static void main(String[] args) {
+        // creates the scanner
         Scanner input = new Scanner(System.in);
         System.out.print("What is your first number and what is the average (between -1000 and 1000): ");
         double numberOne = input.nextDouble();
-        double average = input.nextDouble();
+        double average = input.nextDouble(); // JA: Your variables should be int
 
-        //checks if the number is between the range of -1000 and 1000
-        while( numberOne <= -1000 || average <= -1000 || numberOne >= 1000 || average >= 1000){
+        // checks if the number is between the range of -1000 and 1000
+        while (numberOne <= -1000 || average <= -1000 || numberOne >= 1000 || average >= 1000) {
             System.out.print("What if your first number and what is the average (between -1000 and 1000): ");
             numberOne = input.nextDouble();
             average = input.nextDouble();
         }
 
-        //does the math to find out what the second number is
+        // does the math to find out what the second number is
         double numberTwo = (average * 2) - numberOne;
-        
-        //prints it out to the user
+
+        // prints it out to the user
         System.out.println("Your second number is " + numberTwo + " .");
-
-
-
 
     }
 
